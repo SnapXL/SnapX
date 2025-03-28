04498defb4120748fa39edf7052e90a39a3ea4bc
Brycen G <brycengranville@outlook.com>
2025-03-28 15:30:10 -0400

    build: fix building for Windows ARM64

--- SnapX.Core/DebugHelper.cs
+++ SnapX.Core/DebugHelper.cs
@@ -18,7 +18,6 @@ public static class DebugHelper
         var loggerConfig = new LoggerConfiguration()
 #if DEBUG
             .MinimumLevel.Debug()
-            .WriteTo.Debug()
 #endif
             .Enrich.WithThreadId()
             .Enrich.WithThreadName()
