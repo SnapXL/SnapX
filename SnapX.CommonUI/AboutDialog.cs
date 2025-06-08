3308d2d4bf4fef6abe242e89deb33568fd1b4cbc
Brycen G <brycengranville@outlook.com>
2025-06-07 23:20:42 -0400

    style(CommonUI): add acknowledge for using reflection [ci skip]

--- SnapX.CommonUI/AboutDialog.cs
+++ SnapX.CommonUI/AboutDialog.cs
@@ -33,6 +33,7 @@ public class AboutDialog
     public virtual string GetOsPlatform() => $"{Environment.OSVersion.Platform} {Environment.OSVersion.Version}";
     [DynamicDependency(DynamicallyAccessedMemberTypes.PublicProperties, typeof(ThisAssembly))]
     [RequiresUnreferencedCode("Uses reflection to access properties that may be removed by the trimmer.")]
+    [UnconditionalSuppressMessage("Trimming", "IL2026:Members annotated with 'RequiresUnreferencedCodeAttribute' require dynamic access otherwise can break functionality when trimming application code", Justification = "<Pending>")]
     public virtual string GetBuildInformation()
     {
         var title = Assembly.GetExecutingAssembly().GetCustomAttribute<AssemblyTitleAttribute>()?.Title ?? "Unknown Title";
