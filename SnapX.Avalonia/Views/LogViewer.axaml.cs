accae46bda2e8d3f185466708e2af8db6f1a2403
Brycen G <brycengranville@outlook.com>
2025-06-25 17:03:46 -0400

    feat(Avalonia): add fully functional LogViewer

---
+++ SnapX.Avalonia/Views/LogViewer.axaml.cs
@@ -0,0 +1,191 @@
+
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Documents;
+using Avalonia.Interactivity;
+using Avalonia.Media;
+using Avalonia.Threading;
+using FluentAvalonia.UI.Windowing;
+using Serilog.Events;
+using SnapX.Core;
+using SnapX.Core.Upload;
+using SnapX.Core.Utils;
+
+namespace SnapX.Avalonia.Views;
+
+public partial class LogViewer : AppWindow
+{
+    private ScrollViewer? _scrollViewer;
+    private SelectableTextBlock? _logTextBlock;
+    private int _lastDisplayedLogCount;
+    private DispatcherTimer _refreshTimer;
+    public LogViewer()
+    {
+        InitializeComponent();
+        _refreshTimer = new DispatcherTimer
+        {
+            Interval = TimeSpan.FromMilliseconds(10)
+        };
+        _refreshTimer.Tick += (_, _) => RefreshLogs();
+    }
+
+
+    private void AppWindow_OnLoaded(object? sender, RoutedEventArgs e)
+    {
+        _scrollViewer = this.FindControl<ScrollViewer>("ScrollViewer");
+        _logTextBlock = this.FindControl<SelectableTextBlock>("LogTextBlock");
+        _lastDisplayedLogCount = 0;
+        _refreshTimer.Start();
+    }
+
+    private void AppWindow_OnClosed(object? sender, EventArgs e)
+    {
+        _lastDisplayedLogCount = 0;
+        _refreshTimer.Stop();
+    }
+
+    private void Close_Click(object? sender, RoutedEventArgs e) => Close();
+
+    private void RefreshLogs()
+    {
+        if (DebugHelper.LogEvents.Count() <= _lastDisplayedLogCount) return;
+        for (var i = _lastDisplayedLogCount; i < DebugHelper.LogEvents.Count(); i++)
+        {
+            var logEvent = DebugHelper.LogEvents.ElementAt(i);
+            var timestamp = logEvent.Timestamp.ToString("hh:mm:ss");
+            var level = logEvent.Level.ToString().ToUpper();
+            if (level == "INFORMATION") level = "INFO";
+            var message = logEvent.RenderMessage();
+            if (_logTextBlock is null)
+            {
+                DebugHelper.WriteLine($"{nameof(RefreshLogs)}: {nameof(_logTextBlock)} is null!");
+                return;
+            }
+
+            _logTextBlock.Inlines!.Add(new Run($"{timestamp} ")
+            {
+                Foreground = Brushes.DimGray // Use DimGray for a softer grey that works well on dark backgrounds
+            });
+
+            _logTextBlock.Inlines.Add(new Run($"[{level}] ")
+            {
+                Foreground = GetBrushForLevel(logEvent.Level)
+            });
+
+            _logTextBlock.Inlines.Add(new Run($"{message}\n")
+            {
+                Foreground = Brushes.White
+            });
+
+            if (logEvent.Exception != null)
+            {
+                _logTextBlock.Inlines.Add(new Run(logEvent.Exception + "\n")
+                {
+                    Foreground = Brushes.DarkRed
+                });
+            }
+            _lastDisplayedLogCount = DebugHelper.LogEvents.Count();
+        }
+    }
+    private IBrush GetBrushForLevel(LogEventLevel level)
+    {
+        return level switch
+        {
+            LogEventLevel.Verbose => new LinearGradientBrush
+            {
+                StartPoint = new RelativePoint(0, 0, RelativeUnit.Relative),
+                EndPoint = new RelativePoint(1, 1, RelativeUnit.Relative),
+                GradientStops = new GradientStops
+            {
+                new GradientStop(Color.FromRgb(60, 60, 60), 0),
+                new GradientStop(Color.FromRgb(40, 40, 40), 1)
+            }
+            },
+
+            LogEventLevel.Debug => new LinearGradientBrush
+            {
+                StartPoint = new RelativePoint(0, 0, RelativeUnit.Relative),
+                EndPoint = new RelativePoint(1, 1, RelativeUnit.Relative),
+                GradientStops = new GradientStops
+            {
+                new GradientStop(Color.FromRgb(50, 100, 150), 0),
+                new GradientStop(Color.FromRgb(30, 70, 120), 1)
+            }
+            },
+
+            LogEventLevel.Information => new LinearGradientBrush
+            {
+                StartPoint = new RelativePoint(0, 0, RelativeUnit.Relative),
+                EndPoint = new RelativePoint(1, 1, RelativeUnit.Relative),
+                GradientStops = new GradientStops
+            {
+                new GradientStop(Color.FromRgb(70, 130, 70), 0),
+                new GradientStop(Color.FromRgb(40, 90, 40), 1)
+            }
+            },
+
+            LogEventLevel.Warning => new LinearGradientBrush
+            {
+                StartPoint = new RelativePoint(0, 0, RelativeUnit.Relative),
+                EndPoint = new RelativePoint(1, 1, RelativeUnit.Relative),
+                GradientStops = new GradientStops
+            {
+                new GradientStop(Color.FromRgb(200, 160, 0), 0),
+                new GradientStop(Color.FromRgb(150, 110, 0), 1)
+            }
+            },
+
+            LogEventLevel.Error => new LinearGradientBrush
+            {
+                StartPoint = new RelativePoint(0, 0, RelativeUnit.Relative),
+                EndPoint = new RelativePoint(1, 1, RelativeUnit.Relative),
+                GradientStops = new GradientStops
+            {
+                new GradientStop(Color.FromRgb(180, 50, 50), 0),
+                new GradientStop(Color.FromRgb(130, 30, 30), 1)
+            }
+            },
+
+            LogEventLevel.Fatal => new LinearGradientBrush
+            {
+                StartPoint = new RelativePoint(0, 0, RelativeUnit.Relative),
+                EndPoint = new RelativePoint(1, 1, RelativeUnit.Relative),
+                GradientStops = new GradientStops
+            {
+                new GradientStop(Color.FromRgb(100, 0, 0), 0),
+                new GradientStop(Color.FromRgb(30, 0, 0), 1)
+            }
+            },
+
+            _ => new SolidColorBrush(Colors.DimGray)
+        };
+    }
+
+    private void ClearButton_OnClick(object? Sender, RoutedEventArgs E)
+    {
+        _logTextBlock?.Inlines?.Clear();
+    }
+
+    private void OpenLogFolderButton_OnClick(object? Sender, RoutedEventArgs E)
+    {
+        URLHelpers.OpenURL(Core.SnapX.LogsFolder);
+    }
+
+    private void UploadLogButton_OnClick(object? Sender, RoutedEventArgs E)
+    {
+        UploadManager.UploadText(_logTextBlock.Inlines?.OfType<Run>()
+            .Aggregate("", (current, run) => current + run.Text)!);
+    }
+
+    private void CopyButton_OnClick(object? Sender, RoutedEventArgs E)
+    {
+        Clipboard?.SetTextAsync(_logTextBlock.Inlines?.OfType<Run>().Aggregate("", (current, run) => current + run.Text)!);
+    }
+
+    private void LogTextBlock_OnSizeChanged(object? Sender, SizeChangedEventArgs e)
+    {
+        if (!e.HeightChanged || _scrollViewer is null) return;
+        var shouldScrollToEnd = Math.Abs(_scrollViewer.Offset.Y - _scrollViewer.Extent.Height + _scrollViewer.Viewport.Height) < 5; // Very small tolerance!
+        if (shouldScrollToEnd) _scrollViewer.ScrollToEnd();
+    }
+}
--- SnapX.Avalonia/Views/MainView.axaml.cs
+++ SnapX.Avalonia/Views/MainView.axaml.cs
@@ -1,10 +1,13 @@
 ﻿using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.LogicalTree;
 using CommunityToolkit.Mvvm.Input;
 using FluentAvalonia.UI.Controls;
 using SnapX.Avalonia.ViewModels;
 using SnapX.Core;
 using SnapX.Core.Job;
 using SnapX.Core.Upload;
+using SnapX.Core.Utils;
 using Image = SixLabors.ImageSharp.Image;
 
 namespace SnapX.Avalonia.Views;
@@ -82,5 +85,61 @@ public partial class MainView : UserControl
         }
 
         ExecuteSelectedCaptureActionCommand.Execute(this);
+    }
+
+    private void AboutItem_Pressed(object? Sender, PointerPressedEventArgs E)
+    {
+        App.CreateAboutWindowStatic();
+    }
+
+    private void SettingsItem_Pressed(object? Sender, PointerPressedEventArgs E)
+    {
+        App.CreateSettingsWindowStatic();
+    }
+    private void FindURLOnDescendant(ILogical control)
+    {
+        foreach (var child in control.GetLogicalChildren())
+        {
+            var toolTip = child.FindLogicalDescendantOfType<ToolTip>(true);
+            if (toolTip is null)
+            {
+                FindURLOnDescendant(child);
+            }
+
+            var url = toolTip?.Content as string ?? string.Empty;
+            if (!string.IsNullOrEmpty(url)) URLHelpers.OpenURL(url);
+        }
+    }
+
+    private void DynamicURL_OnPointerPressed(object? Sender, PointerPressedEventArgs E)
+    {
+        DebugHelper.WriteLine($"{nameof(DynamicURL_OnPointerPressed)}: {Sender} {E.Source}");
+        if (Sender is Control control)
+        {
+            // The ToolTip class has a storage of loaded tooltips, however, when a user clicks without hovering for a second the button didn't work.
+            // So I added the second if-clause.
+            if (ToolTip.GetTip(control) is string url)
+            {
+                URLHelpers.OpenURL(url);
+                return;
+            }
+
+            FindURLOnDescendant(control);
+        }
+        else
+        {
+            DebugHelper.WriteLine(
+                $"{nameof(DynamicURL_OnPointerPressed)} called with {Sender} which is not a Control!!");
+        }
+    }
+
+    private void OpenDebugLog(object? Sender, PointerPressedEventArgs E)
+    {
+        var window = new LogViewer();
+        window.Show(App.MyMainWindow!);
+    }
+
+    private void MainView_OnInit(object? Sender, EventArgs E)
+    {
     }
 }
