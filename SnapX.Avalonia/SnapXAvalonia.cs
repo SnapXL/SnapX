fe4366fbe1aa9897fec486fc0b8018cbff88d937
Brycen G <brycengranville@outlook.com>
2025-12-20 18:22:44 -0500

    fix(Avalonia): downgrade FluentIcons to 1.2.315
    Fixes runtime crash

--- SnapX.Avalonia/SnapXAvalonia.cs
+++ SnapX.Avalonia/SnapXAvalonia.cs
@@ -1,3 +1,16 @@
+using Avalonia.Media.Imaging;
+using SixLabors.ImageSharp;
+
 namespace SnapX.Avalonia;
 
-public class SnapXAvalonia : Core.SnapX;
+public class SnapXAvalonia : Core.SnapX
+{
+
+    public Bitmap ConvertImageSharpImgToAvalonia(Image image)
+    {
+        using var memoryStream = new MemoryStream();
+        image.SaveAsPng(memoryStream);
+        memoryStream.Position = 0;
+        return new Bitmap(memoryStream);
+    }
+}
