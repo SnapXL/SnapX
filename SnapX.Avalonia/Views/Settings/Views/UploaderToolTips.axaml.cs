f1529d1020023f3468dfdf0336f76410c8bb591e
Brycen G <brycengranville@outlook.com>
2026-01-22 14:40:42 -0500

    chore(Avalonia): add missing uploader tooltips control

---
+++ SnapX.Avalonia/Views/Settings/Views/UploaderToolTips.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace SnapX.Avalonia.Views.Settings.Views;
+
+public partial class UploaderToolTips : UserControl
+{
+    public UploaderToolTips()
+    {
+        InitializeComponent();
+    }
+}
